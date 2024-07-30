0ea342f900d84e1ee16c9de04cd0a6f00ab70695
geek-samarth <samarthgeek435@gmail.com>
2024-07-30 19:54:01 +0530

    v1.4.2

--- options.php
+++ options.php
@@ -1,5 +1,6 @@
 <?php
 if(!defined('ABSPATH')) exit;
+use Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController;
 
 add_filter( 'manage_users_columns', 'gwslau_user_table_new_column');
 function gwslau_user_table_new_column( $column ) {
@@ -42,7 +43,8 @@ function gwslau_user_table_new_value( $val, $column_name, $user_id ) {
 	return $val;
 }
 
-add_filter( 'manage_edit-shop_order_columns', 'gwslau_order_table_new_column', 20);
+add_filter( 'manage_edit-shop_order_columns', 'gwslau_order_table_new_column');
+add_filter( 'woocommerce_shop_order_list_table_columns', 'gwslau_order_table_new_column');
 function gwslau_order_table_new_column( $columns ) {
 	$options = get_option( 'gwslau_loginas_options' );
 	$reordered_columns = array();
@@ -63,42 +65,53 @@ function gwslau_order_table_new_column( $columns ) {
 	return $reordered_columns;
 }
 
-add_action( 'manage_shop_order_posts_custom_column' , 'gwslau_orders_column_content', 20, 2 );
-function gwslau_orders_column_content( $column, $post_id ){
+add_action( 'manage_shop_order_posts_custom_column' , 'gwslau_post_orders_column_content' );
+function gwslau_post_orders_column_content($colname) {
+	global $the_order; // the global order object
+
+    if ($colname == 'Login-as') {
+        gwslau_orders_column_content($the_order);
+    }
+}
+
+add_action( 'woocommerce_shop_order_list_table_custom_column',  'gwslau_hpos_shop_order_column', 10, 2 );
+function gwslau_hpos_shop_order_column($column, $order) {
+	if ( 'Login-as' !== $column )		return;
+
+    gwslau_orders_column_content($order);
+}
+
+function gwslau_orders_column_content( $order ){
 	$options = get_option( 'gwslau_loginas_options' );
 	if(gwslau_user_conditional($options)){
 		return;
 	}
-	switch ( $column )
-	{
-		case 'Login-as' :
-			$order = wc_get_order($post_id);
-			$user_id = $order->get_user_id();
-			if($user_id != 0){
-				if($user_id == get_current_user_id()){
-					return _e('This user','gwslau_login_as_user');
-				}
-				$user_info = get_userdata($user_id);
-				if($user_info) {
-					$user_roles=$user_info->roles;
-					$user_roles = array_filter(array_map('trim', $user_roles));
-					if(in_array('administrator', $user_roles)){
-						return  _e('Administrator user','gwslau_login_as_user');
-					}
-					$user_name_type = $options['gwslau_loginas_name_show'];
-					$user_name_data = gwslau_get_display_name($user_id, $user_name_type);
-					if(!empty($user_info)){
-						$links = sprintf('<a href="#" class="page-title-action gwslau-login-as-btn" data-user-id="%d" data-admin-id="%d">%s</a>', absint($user_id),absint(get_current_user_id()), __( 'Login as <span>'.$user_name_data.'</span>', 'gwslau_login_as_user' ));
-						return _e($links,'gwslau_login_as_user');
-					}
-				}else{
-					return _e('User not exist.');
-				}
+	
+	// $order = wc_get_order($order);
+	$user_id = $order->get_user_id();
+	if($user_id != 0){
+		if($user_id == get_current_user_id()){
+			return _e('This user','gwslau_login_as_user');
+		}
+		$user_info = get_userdata($user_id);
+		if($user_info) {
+			$user_roles=$user_info->roles;
+			$user_roles = array_filter(array_map('trim', $user_roles));
+			if(in_array('administrator', $user_roles)){
+				return  _e('Administrator user','gwslau_login_as_user');
 			}
-			else{
-				_e('Visitor','gwslau_login_as_user');
+			$user_name_type = $options['gwslau_loginas_name_show'];
+			$user_name_data = gwslau_get_display_name($user_id, $user_name_type);
+			if(!empty($user_info)){
+				$links = sprintf('<a href="#" class="page-title-action gwslau-login-as-btn" data-user-id="%d" data-admin-id="%d">%s</a>', absint($user_id),absint(get_current_user_id()), __( 'Login as <span>'.$user_name_data.'</span>', 'gwslau_login_as_user' ));
+				return _e($links,'gwslau_login_as_user');
 			}
-			break;
+		}else{
+			return _e('User not exist.');
+		}
+	}
+	else{
+		_e('Visitor','gwslau_login_as_user');
 	}
 }
 
@@ -124,18 +137,24 @@ function gwslau_add_personal_options( WP_User $user )
 add_action('add_meta_boxes', 'gwslau_add_login_as_user_metabox');
 function gwslau_add_login_as_user_metabox()
 {
+	$screen = class_exists( '\Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController' ) && wc_get_container()->get( CustomOrdersTableController::class )->custom_orders_table_usage_is_enabled()
+        ? wc_get_page_screen_id( 'shop-order' )
+        : 'shop_order';
+
 	$options = get_option( 'gwslau_loginas_options' );
 	if(!gwslau_user_conditional($options)){
 		if(isset($options['gwslau_loginas_for']) && !empty($options['gwslau_loginas_for'])){
 			if(in_array("order_edit_page", $options['gwslau_loginas_for'])){
-				add_meta_box( 'login_as_user_metabox', __( 'Login as User' ), 'gwslau_login_as_user_metabox', 'shop_order', 'side', 'low');
+				add_meta_box( 'login_as_user_metabox', __( 'Login as User' ), 'gwslau_login_as_user_metabox', $screen, 'side', 'low');
 			}
 		}
 	}
 }
 
-function gwslau_login_as_user_metabox($post){
-	$order = wc_get_order($post->ID);
+function gwslau_login_as_user_metabox($object){
+	$order = is_a( $object, 'WP_Post' ) ? wc_get_order( $object->ID ) : $object;
+	
+	// $order = wc_get_order($post_or_order_object->ID);
 	$user_id = $order->get_user_id();
 	$options = get_option( 'gwslau_loginas_options' );
 	if($user_id != 0){
